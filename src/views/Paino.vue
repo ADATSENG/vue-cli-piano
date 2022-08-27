83f0c35ef583cea4f00c29bcb539946120ec55a2
ADATSENG <d40228112@gmail.com>
2022-08-27 15:39:41 +0800

    add vue config

---
+++ src/views/Paino.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+#home
+  PainoContainer /
+</template>
+
+<script>
+// @ is an alias to /src
+import PainoContainer from '@/components/PainoContainer.vue';
+
+export default {
+  name: 'Paion',
+  components: {
+    PainoContainer,
+  },
+};
+</script>
